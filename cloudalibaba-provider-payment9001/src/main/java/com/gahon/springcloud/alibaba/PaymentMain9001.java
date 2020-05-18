84f613d7dde7ceab26311956e999f653dc26ff76
Gahon <Gahon1995@gmail.com>
2020-05-18 16:57:29 +0800

    feat: 完成nacos注册中心学习

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/gahon/springcloud/alibaba/PaymentMain9001.java
@@ -0,0 +1,22 @@
+package com.gahon.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import javax.swing.*;
+
+/**
+ * @author Gahon
+ * @date 2020/5/18
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+
+}
